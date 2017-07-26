d90bdc3175c89c3503833418b447802a7a4a4738
Khalil <elghazalykhalil@gmail.com>
2017-07-26 12:08:53 -0400

    Update permission code.

--- app/src/main/java/com/example/khalil/myrobot/CentralHub.kt
+++ app/src/main/java/com/example/khalil/myrobot/CentralHub.kt
@@ -93,14 +93,6 @@ class CentralHub : AppCompatActivity() {
                 ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED ||
                 ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
                 ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-
 
             Toast.makeText(this, "Please grant all the permissions needed for this app to " + "function fully.", Toast.LENGTH_LONG).show()
             return
