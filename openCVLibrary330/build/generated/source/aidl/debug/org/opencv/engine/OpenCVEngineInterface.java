3016aa45a03927a698e994d469480ca20919d9c3
AGKhalil <elghazalykhalil@gmail.com>
2018-03-28 18:03:32 -0400

    Build updates for compatibility with gradle 3.0.

--- openCVLibrary330/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary330/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /Users/Khalil/Desktop/Cell Phone Robot Research Project/Updated code/Cellphone_Robot/openCVLibrary330/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: /home/robond/Desktop/Android_Repos/Cellphone_Robot/openCVLibrary330/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
